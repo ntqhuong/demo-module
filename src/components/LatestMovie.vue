36249d4f33c45776f8a9bddcdd30a83c6e791afb
Nguyễn Thị Quỳnh Hương <iris.nguyen1309.qh@gmail.com>
2020-11-17 21:26:41 +0700

    Demo Movie App

---
+++ src/components/LatestMovie.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container v-if="loading">
+    <div class="text-xs-center">
+      <v-progress-circular indeterminate :size="150" :width="8" color="green">
+      </v-progress-circular>
+    </div>
+  </v-container>
+  <v-container v-else grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs4 v-for="(item, index) in wholeResponse" :key="index" mb-2>
+        <v-card>
+          <v-img :src="item.Poster" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <div>
+              <h2>{{ item.Title }}</h2>
+              <div>Year: {{ item.Year }}</div>
+              <div>Type: {{ item.Type }}</div>
+              <div>IMDB-id: {{ item.imdbID }}</div>
+            </div>
+          </v-card-title>
+          <v-card-actions class="justify-center">
+            <v-btn color="green" v-on:click="singleMovie(item.imdbID)"
+              >View</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      wholeResponse: [],
+      loading: true,
+    };
+  },
+  methods: {
+    singleMovie (id) {
+      this.$router.push('/movie/' + id)
+    }
+  },
+  mounted() {
+    axios
+      .get(
+        "http://www.omdbapi.com/?s=iron&apikey=8d762b08&page=1&type=movie&Content-Type=application/json"
+      )
+      .then((response) => {
+        this.wholeResponse = response.data.Search;
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+<style lang="scss" scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import LatestMovie from "@/components/LatestMovie";
+import Movie from "@/components/Movie";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/latestmovie",
+    name: "LatestMovie",
+    component: LatestMovie,
+  },
+  {
+    path: "/movie/:id",
+    name: "Movie",
+    props: true,
+    component: Movie,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
